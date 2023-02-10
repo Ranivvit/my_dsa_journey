902a77ba201f4372bdffa04312594187e677bb87
Manish <mahivvit@gmail.com>
2023-02-11 02:07:38 +0530

    final commit

--- arrayreverse.cpp
+++ arrayreverse.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 int manish(){
+    //for key
+    int k,size;
+    cin>>k;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin>>arr[i];
+
+    }
+    reverse(arr,arr+size);
+    cout<<"Reversed array is"<<endl;
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    
 
 
 return 0;
