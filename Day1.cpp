ced77cedc6efe991131a2ee2381e60e4b4e66290
Manish <mahivvit@gmail.com>
2023-02-11 02:10:17 +0530

    final commit

---
+++ Day1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        long h,x,y;
+        cin>>h>>x>>y;
+        
+        if(x>y){
+            
+        std::cout << 1 << std::endl;}
+        else{
+            std::cout << 0<< std::endl;
+        }
+        
+    }
+
+
+	return 0;
+}
